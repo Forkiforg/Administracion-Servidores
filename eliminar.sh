c45e23476295c3f26210c9729e86724e731ee778
Brandon <bhp52576@gmail.com>
2025-03-02 17:43:29 -0600

    scripts

---
+++ eliminar.sh
@@ -0,0 +1,7 @@
+clear 
+
+mv $1 /home/brandon/Documentos/cuartoSem/Servidores/.basura/
+
+sleep 2
+echo "el archivo" $1 "ha desaparecido"
+echo pwd $1
---
+++ recuperar.sh
@@ -0,0 +1,7 @@
+clear 
+sleep 1 
+
+mv /home/brandon/Documentos/cuartoSem/Servidores/.basura/$1 /home/brandon/Documentos/cuartoSem/Servidores/Recuperado/
+
+echo "El archivo" $1 "fue recuperado correctamente"
+
